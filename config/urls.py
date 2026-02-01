f8857910a525535ac4cb7928175c0813da1d6212
Aravindha-k <siddhar91@gmail.com>
2026-02-01 20:09:56 +0530

    Serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     path("api/notifications/", include("notifications.urls")),
 ]
 
-
+# ✅ Serve media files properly
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
